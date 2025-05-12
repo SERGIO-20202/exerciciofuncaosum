9d6b1d7343fc750334701e2b7bd8e799d4712a28
SERGIO-20251 <84400826+SERGIO-20202@users.noreply.github.com>
2025-05-11 23:45:44 -0300

    Add files via upload

---
+++ sum.js
@@ -0,0 +1,8 @@
+// Esta função recebe dois números e retorna a soma deles
+function sum(a, b) {
+  return a + b;
+}
+
+// Exemplos de uso
+console.log(sum(3, 4)); // 7
+console.log(sum(10, 15)); // 25
